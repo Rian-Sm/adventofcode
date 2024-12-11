b38b1346ed626dd60094b69055fa13c49954a713
Rian S. Macedo <riansm100@gmail.com>
2024-12-11 10:32:09 -0300

    adicionado classLib de acesso de dados externos

---
+++ 2024/AOC-Data/AOCAccessData.cs
@@ -0,0 +1,19 @@
+﻿namespace AOC_Data;
+
+public class AOCAccessData
+{
+    public static async Task<string?> GetData(){
+
+        var environment = Environment.GetEnvironmentVariables();
+
+        string cookie = environment["COOKIE"].ToString();
+        string url = environment["URL"].ToString();
+
+        HttpClient client = new HttpClient();
+        client.DefaultRequestHeaders.Add("cookie", cookie);
+        var response = await client.GetAsync(url);
+        string responseString = await response.Content.ReadAsStringAsync();
+
+        return responseString;
+    }
+}
