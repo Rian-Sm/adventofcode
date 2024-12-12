83092031c53eb1d7aa02fc7b4bf6c8310aa1d4ca
Rian S. Macedo <riansm100@gmail.com>
2024-12-12 20:35:15 -0300

    criado teste da classLib AOC-Data

---
+++ 2024/AOC-Data.Tests/AOCAccessDataTest.cs
@@ -0,0 +1,68 @@
+namespace AOC_Data.Tests;
+
+public class LaunchSettingsFixture { 
+    public LaunchSettingsFixture() { 
+        using (var file = File.OpenText("Properties/launchSettings.json")) 
+        { 
+            var reader = new JsonTextReader(file); 
+            var jObject = JObject.Load(reader);
+            var variables = jObject["profiles"]["local"]["environmentVariables"];
+            foreach (var variable in variables.Children<JProperty>())
+            { 
+                // Console.WriteLine(variable.Value);
+                Environment.SetEnvironmentVariable(variable.Name, variable.Value.ToString()); 
+            } 
+        }
+    }
+}
+
+[CollectionDefinition("LaunchSettings collection")] 
+public class LaunchSettingsCollection : ICollectionFixture<LaunchSettingsFixture> { 
+    // This class has no code, and is never created. Its purpose is simply
+    // to be the place to apply [CollectionDefinition] and all the 
+    // ICollectionFixture<> interfaces.
+}
+
+[Collection("LaunchSettings collection")]
+public class AOCAccessDataTest
+{
+    [Fact]
+    public async void TestErrorOnGetData()
+    {
+        // Arrange
+        Environment.SetEnvironmentVariable("URL", "https://adventofcode.com/2024/day/1/input");
+        Environment.SetEnvironmentVariable("COOKIE", "https://adventofcode.com/2024/day/1/input");
+        string messageErro = "Puzzle inputs differ by user.  Please log in to get your puzzle input.\n";
+        
+        // Act 
+
+        string result =  await AOCAccessData.GetData();
+
+        // Assert
+
+        Assert.Equal(messageErro, result);
+    }
+
+    [Fact]
+    public async void TestGetData()
+    {
+        // Arrange
+        string path = $"{System.IO.Directory.GetCurrentDirectory()}/StaticFiles/mapTest.txt";
+        string textComparation = "";
+
+        using (StreamReader reader = new StreamReader(path))
+            {
+                textComparation = reader.ReadToEnd();
+            }
+
+        string messageErro = "Puzzle inputs differ by user.  Please log in to get your puzzle input.\n";
+        
+        // Act 
+
+        string result = await AOCAccessData.GetData();
+
+        // Assert
+
+        Assert.Equal(textComparation, result);
+    }
+}
---
+++ 2024/AOC-Data.Tests/GlobalUsings.cs
@@ -0,0 +1,5 @@
+global using Xunit;
+global using AOC_Data;
+global using System.IO;
+global using Newtonsoft.Json;
+global using Newtonsoft.Json.Linq;
